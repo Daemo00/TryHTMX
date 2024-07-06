b71d814f949434d15550ff3d93484f9bbed25e15
Simone Rubino <daemo00@gmail.com>
2024-07-06 13:05:32 +0200

    Populate DB

--- src/TryHTMX/orm/main.py
+++ src/TryHTMX/orm/main.py
@@ -2,19 +2,22 @@
 from sqlmodel import create_engine
 from sqlmodel.main import SQLModel
 from sqlmodel.orm.session import Session
-
-
-def populate():
-    """Create records."""
-    return []
-
+from models.hero import populate as hero_populate
 
 engine = create_engine("sqlite://database.db")
 SQLModel.metadata.create_all(engine)
 
-records = populate()
 
-with Session(engine) as session:
-    for record in records:
-        session.add(record)
-    session.commit()
+def _populate_records():
+    """Get records for `populate`."""
+    return hero_populate()
+
+
+def populate():
+    """Create data in DB."""
+    populate_records = _populate_records()
+
+    with Session(engine) as session:
+        for record in populate_records:
+            session.add(record)
+        session.commit()
